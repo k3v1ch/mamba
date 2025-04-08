cffdcf1c2ce46d72deebf99abd1e5976418e5881
k3v1ch <vip.luckyfrost@mail.ru>
2025-04-08 19:28:44 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let temperature = Number(prompt("Какая темп на улице"));
+let message = temperature > 20
+    ? "На улице тепло, можно идти нахуй"
+    : "На улице холодно, можешь идти нахуй смело";
+console.log(message);
