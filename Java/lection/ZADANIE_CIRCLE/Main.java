cea7af1bb242bef2194cd0f1431993fa13e388f8
k3v1ch <vip.luckyfrost@mail.ru>
2025-09-25 20:06:13 +1000

    123

--- Java/lection/ZADANIE_CIRCLE/Main.java
+++ Java/lection/ZADANIE_CIRCLE/Main.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle();
-        Rectangle rectangle = new Rectangle();
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Shape> Shapes = new ArrayList<>();
-        Shapes.add(circle);
-        Shapes.add(rectangle);
-
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Выбирете фигуру:");
+        System.out.println("1 - круг");
+        System.out.println("2 - прямоугольник");
         int choice = sc.nextInt();
         Shape shape;
         if (choice == 1){
-
+            System.out.println("Введите радиус круга");
+            double r = sc.nextDouble();
+            shape = new Circle(r);
+        } else {
+            System.out.println("Введите ширину");
+            double w = sc.nextDouble();
+            System.out.println("Введите высоту");
+            double h = sc.nextDouble();
         }
+        System.out.println("Площадб выбранной фигуры:"+ shape.area() );
     }
 }
--- Java/lection/ZADANIE_CIRCLE/Rectangle.java
+++ Java/lection/ZADANIE_CIRCLE/Rectangle.java
@@ -1,10 +1,12 @@
 public class Rectangle implements  Shape{
   double w,h;
-  Rectangle(double w,double h){}
+
+    Rectangle(double w,double h){
+        this.w = w;
+        this.h = h;
+    }
     @Override
     public double area() {
-
-
-
+return w * h;
     }
 }
