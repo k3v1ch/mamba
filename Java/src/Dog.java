611abd36052f09e3f7c519a38a1ca574a6a41db8
k3v1ch <vip.luckyfrost@mail.ru>
2025-09-25 16:25:21 +1000

    123

---
+++ Java/src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("Meat");
+    }
+    public void sound(){
+        System.out.println("gav gav");
+    }
+    public void run(){
+        System.out.println("beghizt");
+    }
+}
